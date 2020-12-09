72c21b6556b04790c925492155e7ed87c9e41634
ivlytvynets <ivlytvynets@users.noreply.github.com>
2020-12-09 18:47:03 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace Sockets
+{
+    partial class Client
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.passwordPicker = new System.Windows.Forms.TextBox();
+            this.passwordInfoLabel = new System.Windows.Forms.Label();
+            this.submitBtn = new System.Windows.Forms.Button();
+            this.connectBtn = new System.Windows.Forms.Button();
+            this.lengthLabel = new System.Windows.Forms.Label();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.attemptLabel = new System.Windows.Forms.Label();
+            this.infoBtn = new System.Windows.Forms.Button();
+            this.infoLabel = new System.Windows.Forms.Label();
+            this.timeLabel = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            // 
+            // passwordPicker
+            // 
+            this.passwordPicker.Enabled = false;
+            this.passwordPicker.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passwordPicker.Location = new System.Drawing.Point(78, 167);
+            this.passwordPicker.Name = "passwordPicker";
+            this.passwordPicker.Size = new System.Drawing.Size(229, 38);
+            this.passwordPicker.TabIndex = 0;
+            this.passwordPicker.Visible = false;
+            this.passwordPicker.KeyDown += new System.Windows.Forms.KeyEventHandler(this.passwordPicker_KeyDown);
+            // 
+            // passwordInfoLabel
+            // 
+            this.passwordInfoLabel.AutoSize = true;
+            this.passwordInfoLabel.Location = new System.Drawing.Point(75, 147);
+            this.passwordInfoLabel.Name = "passwordInfoLabel";
+            this.passwordInfoLabel.Size = new System.Drawing.Size(69, 17);
+            this.passwordInfoLabel.TabIndex = 1;
+            this.passwordInfoLabel.Text = "Password";
+            this.passwordInfoLabel.Visible = false;
+            // 
+            // submitBtn
+            // 
+            this.submitBtn.Location = new System.Drawing.Point(326, 167);
+            this.submitBtn.Name = "submitBtn";
+            this.submitBtn.Size = new System.Drawing.Size(78, 38);
+            this.submitBtn.TabIndex = 2;
+            this.submitBtn.Text = "Start";
+            this.submitBtn.UseVisualStyleBackColor = true;
+            this.submitBtn.Visible = false;
+            this.submitBtn.Click += new System.EventHandler(this.submitBtn_Click);
+            // 
+            // connectBtn
+            // 
+            this.connectBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.connectBtn.Location = new System.Drawing.Point(78, 69);
+            this.connectBtn.Name = "connectBtn";
+            this.connectBtn.Size = new System.Drawing.Size(229, 51);
+            this.connectBtn.TabIndex = 3;
+            this.connectBtn.Text = "Connect to the server";
+            this.connectBtn.UseVisualStyleBackColor = true;
+            this.connectBtn.Click += new System.EventHandler(this.connectBtn_Click);
+            // 
+            // lengthLabel
+            // 
+            this.lengthLabel.AutoSize = true;
+            this.lengthLabel.Location = new System.Drawing.Point(75, 217);
+            this.lengthLabel.Name = "lengthLabel";
+            this.lengthLabel.Size = new System.Drawing.Size(70, 17);
+            this.lengthLabel.TabIndex = 6;
+            this.lengthLabel.Text = "Length is ";
+            this.lengthLabel.Visible = false;
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Location = new System.Drawing.Point(78, 248);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(0, 17);
+            this.resultLabel.TabIndex = 7;
+            this.resultLabel.Visible = false;
+            // 
+            // attemptLabel
+            // 
+            this.attemptLabel.AutoSize = true;
+            this.attemptLabel.Location = new System.Drawing.Point(323, 217);
+            this.attemptLabel.Name = "attemptLabel";
+            this.attemptLabel.Size = new System.Drawing.Size(82, 17);
+            this.attemptLabel.TabIndex = 8;
+            this.attemptLabel.Text = "Attempt №1";
+            this.attemptLabel.Visible = false;
+            // 
+            // infoBtn
+            // 
+            this.infoBtn.Location = new System.Drawing.Point(536, 167);
+            this.infoBtn.Name = "infoBtn";
+            this.infoBtn.Size = new System.Drawing.Size(74, 38);
+            this.infoBtn.TabIndex = 9;
+            this.infoBtn.Text = "Info";
+            this.infoBtn.UseVisualStyleBackColor = true;
+            this.infoBtn.Visible = false;
+            this.infoBtn.Click += new System.EventHandler(this.infoBtn_Click);
+            // 
+            // infoLabel
+            // 
+            this.infoLabel.AutoSize = true;
+            this.infoLabel.Location = new System.Drawing.Point(544, 217);
+            this.infoLabel.Name = "infoLabel";
+            this.infoLabel.Size = new System.Drawing.Size(0, 17);
+            this.infoLabel.TabIndex = 10;
+            this.infoLabel.Visible = false;
+            // 
+            // timeLabel
+            // 
+            this.timeLabel.AutoSize = true;
+            this.timeLabel.Location = new System.Drawing.Point(75, 279);
+            this.timeLabel.Name = "timeLabel";
+            this.timeLabel.Size = new System.Drawing.Size(0, 17);
+            this.timeLabel.TabIndex = 12;
+            this.timeLabel.Visible = false;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // Client
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.timeLabel);
+            this.Controls.Add(this.infoLabel);
+            this.Controls.Add(this.infoBtn);
+            this.Controls.Add(this.attemptLabel);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.lengthLabel);
+            this.Controls.Add(this.connectBtn);
+            this.Controls.Add(this.submitBtn);
+            this.Controls.Add(this.passwordInfoLabel);
+            this.Controls.Add(this.passwordPicker);
+            this.Name = "Client";
+            this.Text = "Client";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.TextBox passwordPicker;
+        private System.Windows.Forms.Label passwordInfoLabel;
+        private System.Windows.Forms.Button submitBtn;
+        private System.Windows.Forms.Button connectBtn;
+        private System.Windows.Forms.Label lengthLabel;
+        private System.Windows.Forms.Label resultLabel;
+        private System.Windows.Forms.Label attemptLabel;
+        private System.Windows.Forms.Button infoBtn;
+        private System.Windows.Forms.Label infoLabel;
+        private System.Windows.Forms.Label timeLabel;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.IO;
+
+namespace Sockets
+{
+    public partial class Client : Form
+    {
+        const int MAX_CHARS = 255;
+        const int _port = 1038;
+
+        const string CAPTION_ERR = "Помилка";
+        const string ERR_TOO_MANY = "Довжина паролю перевищує допустиму";
+        const string _serverIp = "127.0.0.1";
+        const string _charSet = "abcdefghijklmnopqrstuvwxyz";
+        const string _logPath = "E:\\server\\client_log.txt";
+
+        bool _started = false;
+
+        int _passwordLength = 0;
+        int _attemptCount = 1;
+        int _countdown = 100;
+
+        public Client()
+        {
+            InitializeComponent();
+            File.WriteAllText(_logPath, String.Empty);
+        }
+
+        private void Log(string data)
+        {
+            using (StreamWriter writer = File.AppendText(_logPath))
+            {
+                writer.WriteLine(data);
+            }
+        }
+
+        private void connectBtn_Click(object sender, EventArgs e)
+        {
+            Submit("connect", out string reply);
+            Log("Connected to server at " + DateTime.Now.ToString());
+
+            lengthLabel.Text = lengthLabel.Text + reply;
+            _passwordLength = Int32.Parse(reply);
+
+            passwordInfoLabel.Visible = true;
+            passwordPicker.Visible = true;
+            submitBtn.Visible = true;
+            lengthLabel.Visible = true;
+            resultLabel.Visible = true;
+            attemptLabel.Visible = true;
+            infoBtn.Visible = true;
+            infoLabel.Visible = true;
+            timeLabel.Visible = true;
+            connectBtn.Visible = false;
+        }
+
+        private void submitBtn_Click(object sender, EventArgs e)
+        {
+            if (!_started)
+            {
+                submitBtn.Text = "Submit";
+                _started = true;
+                passwordPicker.Enabled = true;
+                InitializeTimer();
+                return;
+            }
+
+            Submit(passwordPicker.Text, out string reply);
+            ProcessAttempt(reply);
+        }
+
+        private void Submit(string message, out string result)
+        {
+
+            if (message.Length >= MAX_CHARS)
+            {
+                MessageBox.Show(ERR_TOO_MANY, CAPTION_ERR, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                result = "";
+                return;
+            }
+
+            try
+            {
+                IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse(_serverIp), _port);
+                Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                socket.Connect(ipPoint);
+
+                byte[] data = Encoding.Unicode.GetBytes(message.Length.ToString() + message);
+                socket.Send(data);
+
+                result = ReceiveServerReply(socket);
+
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+
+            }
+            catch (Exception ex)
+            {
+                result = "";
+            }
+        }
+
+        private string ReceiveServerReply(Socket socket)
+        {
+            byte[] data = new byte[256];
+            StringBuilder builder = new StringBuilder();
+            int bytes = 0;
+
+            do
+            {
+                bytes = socket.Receive(data, data.Length, 0);
+                builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+            }
+            while (socket.Available > 0);
+
+            return builder.ToString();
+        }
+
+        private void passwordPicker_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                string passwordSent = passwordPicker.Text;
+                Submit(passwordSent, out string reply);
+                Log("Password sent to server at " + DateTime.Now.ToString() + ": " + passwordSent);
+                ProcessAttempt(reply);
+            }
+        }
+
+        private bool ProcessAttempt(string reply)
+        {
+            passwordPicker.Text = "";
+            resultLabel.Text = reply;
+            _attemptCount++;
+            attemptLabel.Text = "Attempt №" + _attemptCount.ToString();
+            
+            if (reply.Equals("Password is correct!"))
+            {
+                timer.Stop();
+                passwordPicker.Enabled = false;
+                Log("Password cracked successfully at " + DateTime.Now.ToString() + " in " + _attemptCount + " attempts");
+                return true;
+            }
+            return false;
+        }
+
+
+        private void infoBtn_Click(object sender, EventArgs e)
+        {
+            Submit("who", out string reply);
+            infoLabel.Text = reply;
+            Log("Info retrieved: " + reply);
+        }
+
+        private bool AutoPick(string prefix, int length)
+        {
+
+            if (length == 0)
+            {
+                Submit(prefix, out string reply);
+                Log("Attempt password sent to server at " + DateTime.Now.ToString() + ": " + prefix);
+                if (!timer.Enabled || ProcessAttempt(reply))
+                {
+                    return true;
+                }
+                return false;
+            }
+
+            for (int i = 0; i < _charSet.Length; ++i)
+            {
+                string newPrefix = prefix + _charSet[i];
+                if (AutoPick(newPrefix, length - 1))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private void InitializeTimer()
+        {
+            timer.Tick += new EventHandler(TimerTick);
+            timer.Start();
+        }
+
+        private void TimerTick(object sender, EventArgs e)
+        {
+            _countdown--;
+            timeLabel.Text = "Remaining time: " + _countdown.ToString();
+
+            if (_countdown == 0)
+            {
+                timer.Stop();
+                passwordPicker.Enabled = false;
+                submitBtn.Enabled = false;
+                infoBtn.Enabled = false;
+            }
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            AutoPick("", (int)e.Argument);
+        }
+    }
+}
